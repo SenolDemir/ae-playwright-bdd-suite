a48c03ba9ed0e00abb76e9564ae1a40838507452
“SenolDemir” <senoldemir@ymail.com>
2026-07-26 20:55:12 +0200

    login api test with api objects created

--- src/api-clients/signup.client.ts
+++ src/api-clients/signup.client.ts
@@ -1,9 +1,6 @@
 import type { APIRequestContext, APIResponse } from "@playwright/test";
-import {
-  UserFactory,
-  type SignupPayload,
-  type SignupUser,
-} from "../test-data/UserFactory";
+import { UserFactory, type SignupUser } from "../test-data/UserFactory";
+import type { SignupPayload } from "./api.models";
 
 /**
  * SignupClient encapsulates all signup-related API calls and payload templates.
@@ -15,14 +12,36 @@ export class SignupClient {
     this.request = request;
   }
 
-  static createNewUserPayload(
-    overrides?: Partial<SignupPayload>,
-  ): SignupPayload {
+  static createNewUserPayload(overrides?: Partial<SignupPayload>): SignupPayload {
     const user = UserFactory.generateSignupUser();
-    return UserFactory.toSignupPayload(user, overrides);
+    return SignupClient.toSignupPayload(user, overrides);
+  }
+
+  // transforms SignupUser to API's expected shape (payload)
+  static toSignupPayload(user: SignupUser, overrides?: Partial<SignupPayload>): SignupPayload {
+    return {
+      name: user.fullName,
+      email: user.email,
+      password: user.password,
+      title: user.title,
+      birth_date: user.dayOfBirth ?? "",
+      birth_month: user.monthOfBirth ?? "",
+      birth_year: user.yearOfBirth ?? "",
+      firstname: user.firstName,
+      lastname: user.lastName,
+      company: user.company,
+      address1: user.address1,
+      address2: user.address2,
+      country: user.country,
+      zipcode: user.zipcode,
+      state: user.state,
+      city: user.city,
+      mobile_number: user.mobileNumber,
+      ...overrides,
+    };
   }
   /**
    * const existingEmailPayload = SignupClient
-   *        .toSignupPayload({ email: "existing@test.com" });
+   *        .toSignupPayload(user, { email: "existing@test.com" });
    */
 }
